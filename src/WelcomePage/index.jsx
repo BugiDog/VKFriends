524817c878010358ce18fd5a8eab8037af47b516
Sasha Abramov <sancha0809@inbox.ru>
2020-07-07 22:52:27 +0300

    FirstBuild

--- src/WelcomePage/index.jsx
+++ src/WelcomePage/index.jsx
@@ -2,23 +2,19 @@ import React from "react";
 import "./style.css";
 
 function WelcomePage() {
+  function login() {
+    const loginAcc = fetch(
+      `https://oauth.vk.com/authorize?client_id=1&display=popup&redirect_uri=https://bugidog.github.io/VKFriends/&scope=friends&response_type=token&v=5.120&state=123456`
+    );
 
-function login () {
-  const loginAcc=fetch(`https://oauth.vk.com/authorize?client_id=1&display=popup&redirect_uri=https://vk.com/id604994034&scope=friends&response_type=token&v=5.120&state=123456`,
-  {credentials: 'same-origin'}).then(data => console.log(data))
-
-
-
-
-
-  //const loginAcc=fetch('https://oauth.vk.com/authorize?client_id=1&display=page&redirect_uri=http://example.com/callback&scope=friends&response_type=token&v=5.120&state=123456')
-}
+    //const loginAcc=fetch('https://oauth.vk.com/authorize?client_id=1&display=page&redirect_uri=http://example.com/callback&scope=friends&response_type=token&v=5.120&state=123456')
+  }
 
   return (
     <div className="login_win">
       <input type="text" placeholder="Email" />
-      <input type="password" placeholder="Password"/>
-      <button onClick= {login} >Войти</button>
+      <input type="password" placeholder="Password" />
+      <button onClick={login}>Войти</button>
     </div>
   );
 }
