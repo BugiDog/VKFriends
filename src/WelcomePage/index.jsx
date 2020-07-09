2c4892f8f725408a8e2f0c50f5165105d944c78b
Sasha Abramov <sancha0809@inbox.ru>
2020-07-09 13:05:20 +0300

    BuildCommit

--- src/WelcomePage/index.jsx
+++ src/WelcomePage/index.jsx
@@ -3,10 +3,15 @@ import "./style.css";
 
 function WelcomePage() {
   function login() {
-    const loginAcc = fetch(
-      `https://oauth.vk.com/authorize?client_id=1&display=popup&redirect_uri=https://bugidog.github.io/VKFriends/&scope=friends&response_type=token&v=5.120&state=123456`
-    );
-
+    const redirectUri = "http://localhost:3000/";
+    const url = `https://oauth.vk.com/authorize?client_id=7534348&display=popup&redirect_uri=${redirectUri}&scope=friends&response_type=token&v=5.120`
+    console.log(url);
+    const loginAcc = fetch(url, {
+      mode: "no-cors",
+      method: "GET",
+      credentials: "include",
+    });
+    console.log(loginAcc);
     //const loginAcc=fetch('https://oauth.vk.com/authorize?client_id=1&display=page&redirect_uri=http://example.com/callback&scope=friends&response_type=token&v=5.120&state=123456')
   }
 
